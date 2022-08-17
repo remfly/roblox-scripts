4888a37811b0d66eb7eed3f01fdae0dfd63ce546
remfly <remfly@protonmail.com>
2022-08-17 16:31:20 -0300

    Fix

--- games/6708206173-rate-my-avatar/anti_rate.lua
+++ games/6708206173-rate-my-avatar/anti_rate.lua
@@ -1,7 +1,7 @@
 local render = game:FindService("RunService").RenderStepped;
 
 spawn(pcall(function()
-    while render:wait() do
+    while wait(0.25) do
         local character = game:FindService("Players").LocalPlayer.Character or game:FindService("Players").LocalPlayer.CharacterAdded:wait();
         if character:WaitForChild("HumanoidRootPart"):FindFirstChild("ProximityPrompt") then
 	        character:WaitForChild("HumanoidRootPart"):FindFirstChild("ProximityPrompt"):Destroy();
